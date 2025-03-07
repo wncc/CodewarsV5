c0223db2b8f0eae800ff7a75031349b9f8f9092b
Tushar Singha Roy <tushroy81@gmail.com>
2025-03-08 03:45:37 +0530

    ROY IS BOSS

--- teams/team1.py
+++ teams/team1.py
@@ -1,4 +1,4 @@
 team_name = "DHOLAKPUR"
-troops = ["Balloon","Knight","Archer","Dragon","Skeleton",'Barbarian',"Giant","Archer","Princess","Knight"]
+troops = ["Prince","Balloon","Knight","Archer","Dragon","Skeleton",'Barbarian',"Giant","Archer","Princess","Knight"]
 def deploy(arena_data:dict):
     return [('Archer',(0,100)),('Minion',(50,100)),('Dragon',(25,100))]
