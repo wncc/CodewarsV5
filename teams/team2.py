0e04e722b5a296402296f6ef019ff8c79a9dc9f2
Samarth Aggarwal <samarthaggarwal@Samarths-MacBook-Air-2.local>
2025-03-07 20:12:10 +0530

    cards

--- teams/team2.py
+++ teams/team2.py
@@ -1,4 +1,4 @@
 team_name = "BHEEMPUR"
-troops = ['Barbarian',"Prince","Prince","Archer","Dragon","Giant","BombTower","Princess","Minion","Knight"]
+troops = ['Barbarian',"Prince","Prince","Giant","Dragon","Giant","BombTower","Princess","Minion","Knight"]
 def deploy(arena_data:dict):
-    return [('Barbarian',(25,100))]
+    return [('Barbarian',(25,100)),('Prince',(48,100)),('Giant',(10,100))]
