b7b2aad452d5a32af1f9482efc3e4cad54d423c6
Tushar Singha Roy <tushroy81@gmail.com>
2025-03-11 19:43:34 +0530

    balloon fixed

--- scripts/Troops/troops.py
+++ scripts/Troops/troops.py
@@ -43,7 +43,7 @@ class Princess(Troop):
     def __init__(self, position, images , surf, myTower,std_size, uid):
         super().__init__(name="Princess", images = images, position = position, elixir = 3,
                          health=287, damage=186, velocity=MEDIUM_SPEED, type_="ground", attack_range = 9, surf = surf, discovery_range = 10, myTower = myTower,
-                         target_type = {"air": True, "ground": True, "building": True}, splash_range = 1, size = 0.6,std_size= std_size, uid = uid, attack_speed = MEDIUM_ATTACK)
+                         target_type = {"air": True, "ground": True, "building": True}, splash_range = 1, size = 0.15,std_size= std_size, uid = uid, attack_speed = MEDIUM_ATTACK)
 class Knight(Troop):
     def __init__(self, position, images , surf, myTower,std_size, uid):
         super().__init__(name="Knight", images = images, position = position, elixir = 3,
