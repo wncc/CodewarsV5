0737fb86bf1b0241ad6cd39193e9d6fe737c1aad
Samarth Aggarwal <samarthaggarwal@Samarths-MacBook-Air-2.local>
2025-03-12 09:41:18 +0530

    ranges fix

--- scripts/Troops/troops.py
+++ scripts/Troops/troops.py
@@ -16,7 +16,7 @@ class Giant(Troop):
 class Dragon(Troop):
     def __init__(self,images , position, surf, myTower, std_size, uid):
         super().__init__(name="Dragon", images  = images, position = position, elixir = 4,
-                         health=1267, damage=176, velocity=FAST_SPEED, type_="air", attack_range = 3.5, surf = surf, discovery_range = 5, myTower = myTower,
+                         health=1267, damage=176, velocity=FAST_SPEED, type_="air", attack_range = 3.5, surf = surf, discovery_range = 6, myTower = myTower,
                          target_type = {"air": True, "ground": True, "building": True}, splash_range = 1, size = 0.4, std_size= std_size, uid = uid, attack_speed = FAST_ATTACK)
 class Balloon(Troop):
     def __init__(self,images , position, surf, myTower, std_size, uid):
@@ -26,7 +26,7 @@ class Balloon(Troop):
 class Prince(Troop):
     def __init__(self, position, images , surf, myTower,std_size, uid):
         super().__init__(name="Prince", images = images, position = position, elixir = 5,
-                         health=1920, damage=392, velocity=FAST_SPEED, type_="ground", attack_range = 0, surf = surf, discovery_range = 5, myTower = myTower,
+                         health=1920, damage=392, velocity=FAST_SPEED, type_="ground", attack_range = 0, surf = surf, discovery_range = 6, myTower = myTower,
                          target_type = {"air": False, "ground": True, "building": False}, splash_range = 0, size = 0.3,std_size= std_size, uid = uid, attack_speed = FAST_ATTACK)
 class Barbarian(Troop):
     def __init__(self, position, images , surf, myTower,std_size, uid):
@@ -47,7 +47,7 @@ class Knight(Troop):
 class Minion(Troop):
     def __init__(self, position, images , surf, myTower,std_size, uid):
         super().__init__(name="Minion", images = images, position = position, elixir = 5,
-                         health=252, damage=129, velocity=FAST_SPEED, type_="air", attack_range = 2, surf = surf, discovery_range = 4, myTower = myTower,
+                         health=252, damage=129, velocity=FAST_SPEED, type_="air", attack_range = 2, surf = surf, discovery_range = 6, myTower = myTower,
                          target_type = {"air": True, "ground": True, "building": True}, splash_range = 0, size = 0.15,std_size= std_size, uid = uid, attack_speed = FAST_ATTACK,
                          number=3) 
 class Skeleton(Troop):
