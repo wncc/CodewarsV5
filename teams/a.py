224ed4b4ad6cb674e6ac214bb135c8d5fbf3cf77
Tushar Singha Roy <tushroy81@gmail.com>
2025-03-17 09:50:03 +0530

    removed comments

--- teams/a.py
+++ teams/a.py
@@ -1,25 +1,3 @@
-# from teams.helper_function import Troops, Utils
-
-# team_name = "MUMBAI"
-# troops = [Troops.prince,Troops.minion,Troops.archer,Troops.giant,Troops.dragon,Troops.skeleton,Troops.balloon,Troops.barbarian]
-# deploy_list = Troops([])
-# team_signal = "h"
-
-# def deploy(arena_data:dict):
-#     """
-#     DON'T TEMPER DEPLOY FUCNTION
-#     """
-#     deploy_list.list_ = []
-#     logic(arena_data)
-#     return deploy_list.list_, team_signal
-
-# def logic(arena_data:dict):
-#     global team_signal
-#     deploy_list.deploy_prince((-16,0))
-#     """
-#     WRITE YOUR CODE HERE 
-#     """
-
 import random
 from teams.helper_function import Troops, Utils
 
--- teams/b.py
+++ teams/b.py
@@ -1,22 +1,3 @@
-# from teams.helper_function import Troops, Utils
-
-# team_name = "DELHI"
-# troops = [Troops.dragon,Troops.skeleton,Troops.wizard,Troops.minion,Troops.archer,Troops.giant,Troops.balloon,Troops.barbarian]
-# deploy_list = Troops([])
-# team_signal = ""
-
-# def deploy(arena_data:dict):
-#     """
-#     DON'T TEMPER DEPLOY FUCNTION
-#     """
-#     deploy_list.list_ = []
-#     logic(arena_data)
-#     return deploy_list.list_, team_signal
-
-# def logic(arena_data:dict):
-#     global team_signal
-#     deploy_list.deploy_dragon((-16,0))
-
 from teams.helper_function import Troops, Utils
 
 team_name = "Priyam"
